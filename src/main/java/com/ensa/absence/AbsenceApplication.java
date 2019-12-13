abb8f1de2af4a89ef7a24183cbd4ce9606210a58
naoufal450 <naoufal450@gmail.com>
2019-12-13 22:38:19 +0100

    Set Jackson Timezone to UTC

--- src/main/java/com/ensa/absence/AbsenceApplication.java
+++ src/main/java/com/ensa/absence/AbsenceApplication.java
@@ -1,23 +1,32 @@
 package com.ensa.absence;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+@SpringBootApplication(scanBasePackageClasses = { AbsenceApplication.class, Jsr310JpaConverters.class })
 
-@SpringBootApplication(scanBasePackages = {"com.ensa.absence", "springboot.rest"})
 public class AbsenceApplication {
-	
-  
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AbsenceApplication.class, args);
 	}
-	
+
 	@Bean
-    CommandLineRunner runner() {
-      return args -> {
-      };
-      
+	CommandLineRunner runner() {
+		return args -> {
+		};
+
 	}
 }
