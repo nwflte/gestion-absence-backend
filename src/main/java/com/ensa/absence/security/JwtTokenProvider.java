0f70b0a7901cfee8282ffcb585151d44e7e9e8f7
naoufal450 <naoufal450@gmail.com>
2020-01-15 18:04:12 +0100

    Put jwt constants in properties file

--- src/main/java/com/ensa/absence/security/JwtTokenProvider.java
+++ src/main/java/com/ensa/absence/security/JwtTokenProvider.java
@@ -3,9 +3,9 @@ package com.ensa.absence.security;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
 @Component
@@ -13,11 +13,11 @@ public class JwtTokenProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
-    @Value("${app.jwtSecret}")
-    private String jwtSecret;
+    //@Value("${app.jwtSecret}")
+    private final String jwtSecret = "JWTSuperSecretKey";
 
-    @Value("${app.jwtExpirationInMs}")
-    private int jwtExpirationInMs;
+    //@Value("${app.jwtExpirationInMs}")
+    private final int jwtExpirationInMs = 604800000;
 
     public String generateToken(Authentication authentication) {
 
