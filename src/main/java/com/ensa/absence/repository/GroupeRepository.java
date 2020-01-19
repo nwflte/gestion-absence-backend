5b3a0c98be52dc08a242244c89bd79d368db750e
naoufal450 <naoufal450@gmail.com>
2020-01-19 13:19:18 +0100

    Add findByFiliereAndCategorie method

--- src/main/java/com/ensa/absence/repository/GroupeRepository.java
+++ src/main/java/com/ensa/absence/repository/GroupeRepository.java
@@ -1,13 +1,15 @@
 package com.ensa.absence.repository;
 
-import java.util.List;
-
 import com.ensa.absence.domain.entity.Filiere;
 import com.ensa.absence.domain.entity.Groupe;
-import com.ensa.absence.payload.GroupeResponse;
+import com.ensa.absence.domain.enums.GroupeCategorie;
+
+import java.util.List;
 
 public interface GroupeRepository extends BaseRepository<Groupe, Long> {
 	List<Groupe> findByFiliere(Filiere filiere);
 
 	List<Groupe> findByFiliere_Id(Long filierefId);
+
+	List<Groupe> findByFiliereAndCategorie(Filiere filiere, GroupeCategorie categorie);
 }
